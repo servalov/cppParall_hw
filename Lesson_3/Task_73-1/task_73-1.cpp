364c1f0b5c82d4a5ccd5ff99c26b5150568ef761
Sergey Valov <sergey.valov.eng@gmail.com>
2025-10-13 18:31:02 +0300

    Добавлено домашнее задание к занятию «Асинхронное программирование».

---
+++ Lesson_3/Task_73-1/task_73-1.cpp
@@ -0,0 +1,57 @@
+﻿//Занятие 3. «Асинхронное программирование». Задание 1.Сортировка выбором
+#include <iostream>
+#include <vector>
+#include <random>
+#include <future>
+#include <thread>
+#include <algorithm>
+
+// поиск минимального элемента в векторе
+void min_vecror(std::vector<int>& arr, int ind_begin, std::promise<int> prm)
+{
+	int min_ind = ind_begin;
+	for (int i= ind_begin+1;i < arr.size(); ++i)
+	{
+		if (arr[i] < arr[min_ind])
+		{
+			min_ind = i;
+		}
+	}
+	prm.set_value(min_ind);
+}
+
+void print(int value)
+{
+	std::cout << value << " ";
+}
+
+int main()
+{
+	setlocale(LC_ALL,"Russian");
+	int n{ 10 };
+	std::vector<int> arr(n);
+
+	std::random_device rd;
+	std::mt19937 gen{rd()};
+	std::uniform_int_distribution<int> dis(0, 1000);
+	auto rand_num([=]() mutable {return dis(gen); });
+	generate(arr.begin(), arr.end(), rand_num);
+
+	std::cout << "Исходный массив:" << std::endl;
+	std::for_each(arr.begin(), arr.end(), print);
+
+	for (int i{}; i < arr.size()-1; ++i)
+	{
+		std::promise<int> Promise;
+		std::future<int> Future = Promise.get_future();
+		auto res = std::async(min_vecror, std::ref(arr), i, move(Promise));
+
+		Future.wait();
+		std::swap(arr[i], arr[Future.get()]);
+	}
+
+	std::cout << "\nОтсортированный массив:" << std::endl;
+	std::for_each(arr.begin(), arr.end(), print);
+	
+	return EXIT_SUCCESS;
+}
---
+++ Lesson_3/Task_73-2/task_73-2.cpp
@@ -0,0 +1,56 @@
+﻿// Занятие 3. Асинхронное программирование. Задание 2. For_each
+#include <iostream>
+#include <chrono>
+#include <future>
+
+template<typename Iterator,typename Func>
+void parallel_for_each(Iterator first, Iterator last, Func f)
+{
+	const auto min_size = 10;
+	auto size = distance(first, last);
+	if (size <= min_size)
+	{
+		std::for_each(first, last, std::ref(f));
+		return;
+	}
+	
+	Iterator mid_p = first;
+	advance(mid_p, size / 2);
+	auto first_half = std::async(parallel_for_each <Iterator, Func>, first, mid_p, std::ref(f));
+	//parallel_for_each(mid_p, last, std::ref(f));
+	auto second_half = std::async(parallel_for_each <Iterator, Func>, mid_p, last, std::ref(f));
+}
+
+void change_element(int& n) 
+{
+	n += 1;
+}
+void print_element(int n)
+{
+	std::cout << n << "   ";
+}
+
+int main()
+{	
+	// Работа с обычным for_each
+	auto start = std::chrono::high_resolution_clock::now();
+	std::vector<int> v1(100, 5);
+	std::for_each(v1.begin(), v1.end(), change_element);
+	std::for_each(v1.begin(), v1.end(), print_element);
+	auto end = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double, std::milli> time = end - start;
+	std::cout << "\nfor_each time = " << time.count() << std::endl;
+	
+	std::cout << std::endl;
+	
+	// Работа с параллельным вариантом for_each
+	auto start1 = std::chrono::high_resolution_clock::now();
+	std::vector<int> v2(100, 5);
+	parallel_for_each(v2.begin(), v2.end(), change_element);
+    std::for_each(v2.begin(), v2.end(), print_element);
+	auto end1 = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double, std::milli>time1 = end1 - start1;
+	std::cout << "\nparallel_for_each time= " << time1.count() << std::endl;
+	
+	return EXIT_SUCCESS;
+}
